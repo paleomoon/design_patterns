4322c6b072e5931be93d88a4df65c92e8bb1bbd9
hj <hujunchn@163.com>
2020-12-12 17:12:45 +0800

    lost some part of notes, sad

--- code/AbstractFactory/main.cpp
+++ code/AbstractFactory/main.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	//创建属于不同产品族的产品A和产品B
 	AbstractFactory * fc = new ConcreteFactory1();
 	AbstractProductA * pa =  fc->createProductA();
 	AbstractProductB * pb = fc->createProductB();
-	pa->use();
-	pb->eat();
+	pa->use(); //产品族A的特性
+	pb->eat(); //产品族B的特性
 	
+	//创建第二批次产品，可不看
 	AbstractFactory * fc2 = new ConcreteFactory2();
 	AbstractProductA * pa2 =  fc2->createProductA();
 	AbstractProductB * pb2 = fc2->createProductB();
